833092d3304485c59b5a16eeed30ec044cd42ed3
Luis Noguera <lnoguera171@gmail.com>
2020-11-24 17:52:39 -0500

    packages and data loaded to the project

---
+++ Packages Script.R
@@ -0,0 +1,19 @@
+# Load necessary packages
+
+#install.packages('pacmman')
+# remotes::install_github('ewenme/understatr')
+
+pacman::p_load(dplyr, tidyr, janitor, 
+               purrr, tibble, lubridate, 
+               glue, rlang, ggplot2,
+               understatr)
+
+
+Liverpool_vs_Leicester <- get_match_stats(match_id = 14519)
+
+
+write.csv(Liverpool_vs_Leicester, "Games Data/Liverpool_vs_Leicester.csv")
+
+Sevilla_vs_Celta <- get_match_stats(match_id = 14873)
+
+write.csv(Sevilla_vs_Celta, "Games Data/Sevilla_vs_Celta.csv")
