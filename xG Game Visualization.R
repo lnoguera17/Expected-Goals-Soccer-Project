6c4d447f1ed2c8a6d0e7475991fbcd72ad685bd9
Luis Noguera <lnoguera171@gmail.com>
2020-11-25 22:37:40 -0500

    Points to the xG chart added

--- xG Game Visualization.R
+++ xG Game Visualization.R
@@ -10,17 +10,30 @@ game_xG <- Sevilla_vs_Celta %>%
   summarize(xG_sum = cumsum(xG),
             time = time,
             goals = goals,
-            shots = shots) %>% 
+            shots = shots,
+            xG = xG) %>% 
   ungroup() 
+
+
 
 game_xG %>% 
   ggplot(aes(time, xG_sum, color = team_name)) + 
   geom_line() +
+  geom_point(aes(size = goals,
+                 alpha = 0.5,
+                 ), show.legend = F) +
+  geom_text(aes(label = glue("{round(xG,2)}")),  position = position_dodge(width=0.1), color = 'black', show.legend = F, vjust = -0.7) +
+  scale_size_continuous(range = c(0,5)) +
   scale_x_continuous(breaks = seq(0,90, 5),
                      labels = c(0, 5, 10, 15, 20, 25, 30, 35, 40, "HT", 50, 55, 60, 65, 70, 75, 80, 85, "FT")) +
   labs(y = "Expected Goals",
        x = "Time") +
-  theme_minimal()
+  theme_minimal() + 
+  theme(panel.grid.minor = element_blank()) 
+  
+
+
+
 
 
 
