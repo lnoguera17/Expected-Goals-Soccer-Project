39d89b7f7b37e71762e816b7785f5a5dfdef5d55
Luis Noguera <lnoguera171@gmail.com>
2020-11-27 16:57:49 -0500

    label added to the graph - WIP

--- xG Game Visualization.R
+++ xG Game Visualization.R
@@ -11,30 +11,27 @@ game_xG <- Sevilla_vs_Celta %>%
             time = time,
             goals = goals,
             shots = shots,
+            player = player,
             xG = xG) %>% 
   ungroup() 
 
-
+?str_c()
 
 game_xG %>% 
   ggplot(aes(time, xG_sum, color = team_name)) + 
-  geom_line() +
-  geom_point(aes(size = goals,
-                 alpha = 0.5,
-                 ), show.legend = F) +
-  geom_text(aes(label = glue("{round(xG,2)}")),  position = position_dodge(width=0.1), color = 'black', show.legend = F, vjust = -0.7) +
+  geom_line(aes(alpha = 1,
+                size = 0.1)) +
+  guides(size = F, alpha = F, color = guide_legend("Team Name")) +
+  geom_point(aes(size = goals)) +
+  geom_text_repel(aes(label = glue("{round(xG,2)}", "xG")), color = 'black', show.legend = F, vjust = -0.7) +
   scale_size_continuous(range = c(0,5)) +
   scale_x_continuous(breaks = seq(0,90, 5),
                      labels = c(0, 5, 10, 15, 20, 25, 30, 35, 40, "HT", 50, 55, 60, 65, 70, 75, 80, 85, "FT")) +
   labs(y = "Expected Goals",
        x = "Time") +
-  theme_minimal() + 
-  theme(panel.grid.minor = element_blank()) 
-  
-
-
-
-
+  theme_minimal(base_size = 12) + 
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "top")
 
 
 
