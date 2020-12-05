9cea9597bfe6e0e20e331f7ca8a07d5096751492
Luis Noguera <lnoguera171@gmail.com>
2020-12-04 19:32:06 -0500

    shots data wrangling right #thanksNICO

--- xG Game Visualization.R
+++ xG Game Visualization.R
@@ -1,28 +1,22 @@
 source("Packages Script.R")
+library(ggrepel)
 
 
+game_xG <- shots_Sevilla_Celta %>% 
+  mutate(team = case_when(h_a == "h" ~ h_team,
+                          TRUE ~ a_team)) %>% 
+  mutate(across(c(minute, xG, X, Y, 
+                  player_id, match_id, season), as.numeric)) %>% 
+  mutate(xG = if_else(is.na(xG), 0, xG))
 
-game_xG <- Sevilla_vs_Celta %>% 
-  arrange(time) %>% 
-  mutate(team_name = case_when(team_id == 138 ~ "Sevilla", 
-                               TRUE ~ "Celta de Vigo")) %>% 
-  group_by(team_name) %>% 
-  summarize(xG_sum = cumsum(xG),
-            time = time,
-            goals = goals,
-            shots = shots,
-            player = player,
-            xG = xG) %>% 
-  ungroup() 
 
-?str_c()
 
 game_xG %>% 
-  ggplot(aes(time, xG_sum, color = team_name)) + 
+  ggplot(aes(minute, xg_sum, color = team)) + 
   geom_line(aes(alpha = 1,
                 size = 0.1)) +
   guides(size = F, alpha = F, color = guide_legend("Team Name")) +
-  geom_point(aes(size = goals)) +
+ # geom_point(aes(size = goals)) +
   geom_text_repel(aes(label = glue("{round(xG,2)}", "xG")), color = 'black', show.legend = F, vjust = -0.7) +
   scale_size_continuous(range = c(0,5)) +
   scale_x_continuous(breaks = seq(0,90, 5),
