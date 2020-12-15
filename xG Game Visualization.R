632a3e30f511880e7340faaf181b1e89c20da911
Luis Noguera <lnoguera171@gmail.com>
2020-12-14 21:05:51 -0500

    sum xg

--- xG Game Visualization.R
+++ xG Game Visualization.R
@@ -2,17 +2,31 @@ source("Packages Script.R")
 library(ggrepel)
 
 
+home_color <- "#bb1628"
+away_color <- "#2d3252"
+
 game_xG <- shots_Sevilla_Celta %>% 
-  mutate(team = case_when(h_a == "h" ~ h_team,
-                          TRUE ~ a_team)) %>% 
+  mutate(team = as.factor(case_when(h_a == "h" ~ h_team,
+                          TRUE ~ a_team))) %>% 
   mutate(across(c(minute, xG, X, Y, 
                   player_id, match_id, season), as.numeric)) %>% 
-  mutate(xG = if_else(is.na(xG), 0, xG))
+  mutate(xG = if_else(is.na(xG), 0, xG)) %>%  
+  ## 6. Add team colors to the row depending on the team.
+  mutate(team_color = if_else(team == h_team, home_color, away_color)) %>% 
+  arrange(id) %>% 
+  separate(player, into = c("firstname", "player"), 
+           sep = "\\s", extra = "merge") %>% 
+  ## players like Fabinho are listed without a last name "Tavares"
+  ## so just add their name in again if NA
+  mutate(player = if_else(is.na(player), firstname, player),
+         ## 11. Set a new and cleaner ID for shots so that it starts at 1 and goes to `n`.
+         id = row_number())
+  
 
 
 
 game_xG %>% 
-  ggplot(aes(minute, xg_sum, color = team)) + 
+  ggplot(aes(minute, xG_sum, color = team)) + 
   geom_line(aes(alpha = 1,
                 size = 0.1)) +
   guides(size = F, alpha = F, color = guide_legend("Team Name")) +
