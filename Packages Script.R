32ee69fa6e1677410b9b590774a83a89e5579338
Luis Noguera <lnoguera171@gmail.com>
2020-12-28 17:05:53 -0500

    Cleaned script for testing only

--- Packages Script.R
+++ Packages Script.R
@@ -9,13 +9,7 @@ pacman::p_load(dplyr, tidyr, janitor,
                understatr)
 
 
-Liverpool_vs_Leicester <- get_match_stats(match_id = 14519)
+# Testing the understatr API
+Sevilla_vs_Celta <- get_match_shots(match_id = 14873)
 
 
-#write.csv(Liverpool_vs_Leicester, "Games Data/Liverpool_vs_Leicester.csv")
-
-Sevilla_vs_Celta <- get_match_stats(match_id = 14873)
-
-#write.csv(Sevilla_vs_Celta, "Games Data/Sevilla_vs_Celta.csv")
-
-shots_Sevilla_Celta <- understatr::get_match_shots(match_id = 14873)
